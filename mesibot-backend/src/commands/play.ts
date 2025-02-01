01030e54396f58b93e6389ba93e074c24b6ef60f
ShakedZrihen <shaked.zrihen@gmail.com>
2025-02-01 18:32:34 +0200

    feat: update cookie file

--- mesibot-backend/src/commands/play.ts
+++ mesibot-backend/src/commands/play.ts
@@ -147,6 +147,7 @@ const fetchAudioUrl = async (url: string): Promise<string | null> => {
       noPlaylist: true,
       noCheckCertificates: true,
       youtubeSkipDashManifest: true,
+      cookies: "./cookies.txt",
       addHeader: [
         "referer: https://www.youtube.com/",
         "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
