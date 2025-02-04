1a20469a0b52b90440bcdbf315995a915fff666d
ShakedZrihen <shaked.zrihen@gmail.com>
2025-02-04 20:20:10 +0200

    chore: test with cookie

--- mesibot-backend/src/services/streaming.ts
+++ mesibot-backend/src/services/streaming.ts
@@ -3,6 +3,15 @@ import playdl from "play-dl";
 import { PROXY_USERNAME, PROXY_PASSWORD } from "../env";
 import { setGlobalDispatcher, ProxyAgent } from "undici";
 
+const YOUTUBE_COOKIES =
+  "VISITOR_INFO1_LIVE=r_1Q_LtvloE; SID=g.a000tAjcgvUIxhSWlYJGbpsUmYtbjRGtHQ-E7Inae3H5cH0Jn4e-U22SdRT0gPJ1Vsz_AHtgngACgYKAdYSARASFQHGX2Mig8fenE2qeNKvsamqkMycChoVAUF8yKozj14qgKtNGR-jMHgevDIe0076; HSID=AT7ij4Ek4_vStlI41; SSID=AwbL13pSOhMYIVE53; APISID=u7siox1T3Rf6COgq/A3-B3IAH4L_9IGLJn; SAPISID=YJyFfOQ81w5xUAMt/AyELwzBi3pJILGxkB;";
+
+playdl.setToken({
+  youtube: {
+    cookie: YOUTUBE_COOKIES
+  }
+});
+
 const PROXY_URL = `http://${PROXY_USERNAME}:${PROXY_PASSWORD}@geo.iproyal.com:12321`;
 
 const proxyAgent = new ProxyAgent(PROXY_URL);
