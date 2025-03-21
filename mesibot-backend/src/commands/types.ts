3a2a1a8d1e9c6e1d8a1b877abbe94decf0195d2f
ShakedZrihen <shaked.zrihen@gmail.com>
2025-03-21 17:17:37 +0200

    chore: add placeholder for slash commands

---
+++ mesibot-backend/src/commands/types.ts
@@ -0,0 +1,14 @@
+export enum OptionType {
+  string = 3
+}
+
+export enum SupportedCommands {
+  play = "play",
+  pause = "pause",
+  resume = "resume",
+  join = "join",
+  ping = "ping",
+  clearQueue = "clear-queue",
+  deleteSong = "delete-song",
+  skip = "skip"
+}
